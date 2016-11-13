9a6a644a67a2bf57c569898cb30521c8571f1705
Cássio Böck <cassio.bock@gmail.com>
2016-11-13 23:28:10 +0000

    added cleanup code

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const path = require('path')
 const replacer = require('./src/replacer')
 const scraper = require('./src/scraper')
 
+const branch = 'gh-pages'
+const url = 'http://localhost:2368'
+const newUrl = 'https://cassiobsilva.github.io/blog/'
+const repo = 'git@github.com:cassiobsilva/blog.git'
+const staticPath = path.join(__dirname, `static${new Date().getTime()}`)
 
 scraper.createStaticFiles(url, staticPath)
   .then(() => replacer.correctUrls(url, newUrl, staticPath))
   .then(() => ghpagesdeploy.publish(staticPath, repo, branch))
+  .then(() => cleanup(staticPath))
   .then(() => console.log('published'))
   .catch(console.log)
--- src/cleanup.js
+++ src/cleanup.js
@@ -1,69 +1,28 @@
 const fs = require('fs')
 const path = require('path')
 
-const getFiles = (directory) => {
+const cleanDirectory = (dir) => {
   return new Promise((resolve, reject) => {
-    fs.readdir(directory, (err, files) => err ? reject(err) : resolve(files))
-  })
-}
+    try {
+      const files = fs.readdirSync(dir)
 
-const isDirectory = (itemPath) => {
-  return new Promise((resolve, reject) => {
-    fs.lstat(itemPath, (err, stats) => err ? reject(err) : resolve(stats.isDirectory()))
-  })
-}
+      files.forEach(file => {
+        const filepath = path.join(dir, file)
+        const stat = fs.statSync(filepath)
 
-const deleteFile = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.unlink(filePath, (err) => err ? reject(err) : resolve())
-  })
-}
-
-const deleteDirectory = (directory) => {
-  return new Promise((reject, resolve) => {
-    fs.rmdir(directory, err => err ? reject(err) : resolve())
-  })
-}
-
-const cleanDirectory = (directory) => {
-  return new Promise((resolve, reject) => {
-    getFiles(directory)
-      .then(files => {
-        if (!files.length) {
-          return resolve()
+        if (stat.isDirectory()) {
+          return cleanDirectory(filepath)
         }
 
-        const directories = []
-
-        const deletePromises = files.map(file => {
-          const currentPath = path.join(directory, file)
-
-          return new Promise((resolve, reject) => {
-            isDirectory(currentPath)
-              .then(isDirectory => {
-                if (isDirectory) {
-                  directories.push(currentPath)
-
-                  cleanDirectory(currentPath)
-                    .then(() => resolve())
-                    .catch(reject)
-
-                  return
-                }
-
-                deleteFile(currentPath)
-                  .then(() => resolve())
-                  .catch(reject)
-              })
-              .catch(reject)
-          })
-        })
-
-        return Promise.all(deletePromises)
-          .then(() => Promise.all(directories.map(deleteDirectory)))
-          .catch(reject)
+        fs.unlinkSync(filepath)
       })
-      .catch(reject)
+
+      fs.rmdirSync(dir)
+
+      return resolve()
+    } catch (e) {
+      return reject(e)
+    }
   })
 }
 
