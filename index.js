51c77b89e690205841fbcddfafd991cf8bd63cfd
Cássio Böck <cassio.bock@gmail.com>
2016-11-14 01:40:09 +0000

    package published

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
 const cleanup = require('./src/cleanup')
 const ghpagesdeploy = require('./src/gh-pages-deploy')
 const path = require('path')
+const program = require('commander')
 const replacer = require('./src/replacer')
 const scraper = require('./src/scraper')
 
-const branch = 'gh-pages'
-const url = 'http://localhost:2368'
-const newUrl = 'https://cassiobsilva.github.io/blog/'
-const repo = 'git@github.com:cassiobsilva/blog.git'
-const staticPath = path.join(__dirname, `static${new Date().getTime()}`)
+program
+  .version('0.0.1')
+  .option('-g, --ghost-url [url]', 'configure the Ghost local URL. Default: http://localhost:2368')
+  .option('-r, --github-repo <repo>', 'configure the GitHub repository')
+  .option('-b, --branch <branch>', 'configure the GitHub branch. Default: `gh-pages`')
+  .option('-u, --site-url <siteurl>', 'configure the site URL that will used for the links')
+  .option('-d, --path [path]', `configure the static path. Default: ${path.join(__dirname, 'static')}`)
+  .parse(process.argv)
 
-scraper.createStaticFiles(url, staticPath)
-  .then(() => replacer.correctUrls(url, newUrl, staticPath))
-  .then(() => ghpagesdeploy.publish(staticPath, repo, branch))
+if (!program.githubRepo) {
+  console.error(`Missing ${chalk.red.bold('GitHub repository')} parameter, use ${chalk.yellow('`-r`')} or ${chalk.yellow('`--github-repo`')}`)
+  process.exit(1)
+} else if (!program.branch) {
+  console.error(`Missing ${chalk.red.bold('branch')} parameter, use ${chalk.yellow('`-b`')} or ${chalk.yellow('`--branch`')}`)
+  process.exit(1)
+} else if (!program.siteUrl) {
+  console.error(`Missing site ${chalk.red.bold('url')} parameter, use ${chalk.yellow('`-u`')} or ${chalk.yellow('`--site-url`')}`)
+  process.exit(1)
+}
+
+const ghostUrl = program.ghostUrl || 'http://localhost:2368'
+const staticPath = program.path || path.join(__dirname, 'static')
+
+scraper.createStaticFiles(ghostUrl, staticPath)
+  .then(() => replacer.correctUrls(ghostUrl, program.siteUrl, staticPath))
+  .then(() => ghpagesdeploy.publish(staticPath, program.githubRepo, program.branch))
   .then(() => cleanup(staticPath))
-  .then(() => console.log('published'))
+  .then(() => console.log(chalk.green(`Static Ghost published to ${chalk.bold(program.siteUrl)}`)))
   .catch(console.log)
